3e3b51972962a286462de276214cf8c48be507a9
Mayank Goyal <7029361+imtrinity94@users.noreply.github.com>
2021-12-09 10:12:59 +0530

    Update createNewHVPool.ps1

--- PS_scripts/createNewHVPool.ps1
+++ PS_scripts/createNewHVPool.ps1
@@ -1,8 +1,8 @@
-Connect-HVServer -server s365d1vrexmc01.in365.corpintra.net -user a365_s_osdeploy@apac.corpdir.net -password D!cv@dm!N%9876543210 -Domain apac.corpdir.net
+Connect-HVServer -server horizon_fqdn -user admin@dom.ain -password password -Domain dom.ain
 $poolName = $args[0]
 $pattern = $args[1]
 $maxMachine = $args[2]
 $template = $args[3]
 $custSpec = $args[4]
-(get-content C:\Users\a365_s_osdeploy\Documents\vRAExecutionSetup\fullCloneTemplate.json) | foreach-object {$_ -replace "#poolName", $poolName} |  foreach-object {$_ -replace "#pattern", $pattern} |  foreach-object {$_ -replace "#maxMachine", $maxMachine} | foreach-object {$_ -replace "#template", $template} | foreach-object {$_ -replace "#custSpec", $custSpec} | set-content C:\Users\a365_s_osdeploy\Documents\TempFiles\createPool.json
-New-HVPool -Spec C:\Users\a365_s_osdeploy\Documents\TempFiles\createPool.json -Confirm:$false
+(get-content C:\Users\tempUser\Documents\vRAExecutionSetup\fullCloneTemplate.json) | foreach-object {$_ -replace "#poolName", $poolName} |  foreach-object {$_ -replace "#pattern", $pattern} |  foreach-object {$_ -replace "#maxMachine", $maxMachine} | foreach-object {$_ -replace "#template", $template} | foreach-object {$_ -replace "#custSpec", $custSpec} | set-content C:\Users\tempUser\Documents\TempFiles\createPool.json
+New-HVPool -Spec C:\TempFiles\createPool.json -Confirm:$false
